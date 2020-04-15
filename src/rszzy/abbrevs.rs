23b674fdc072c45ad80993d3a353026296d07ed7
George Madrid <gmadrid@gmail.com>
2020-04-15 16:55:29 -0400

    fehlerizing

--- src/rszzy/abbrevs.rs
+++ src/rszzy/abbrevs.rs
@@ -2,24 +2,27 @@ use super::addressing::WordAddress;
 use super::constants::header_offset::ABBREV_TABLE_START;
 use super::traits::{AbbrevTable, Memory};
 use crate::rszzy::addressing::ZOffset;
-use anyhow::Result;
+use anyhow::Error;
+use fehler::throws;
 
 // Offset is location of abbrev table from header.
 pub struct ZAbbrevTable(ZOffset);
 
 impl ZAbbrevTable {
-    pub fn new(memory: &impl Memory) -> Result<ZAbbrevTable> {
-        Ok(ZAbbrevTable(ZOffset::from(
+    #[throws]
+    pub fn new(memory: &impl Memory) -> ZAbbrevTable {
+        ZAbbrevTable(ZOffset::from(
             memory.read_word(ZOffset::from(ABBREV_TABLE_START))?,
-        )))
+        ))
     }
 }
 
 impl AbbrevTable for ZAbbrevTable {
+    #[throws]
     /// table and idx are as described in ZSpec 3.3.
-    fn abbrev_location(&self, memory: &impl Memory, table: u8, idx: u8) -> Result<WordAddress> {
+    fn abbrev_location(&self, memory: &impl Memory, table: u8, idx: u8) -> WordAddress {
         let offset = self.0 + usize::from(2 * (32 * (table - 1) + idx));
         let abbrev_offset = memory.read_word(offset)?;
-        Ok(abbrev_offset.into())
+        abbrev_offset.into()
     }
 }
